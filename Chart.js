b4bb0af7f2aa6583fec2b206780634cbc22c4498
jrh16u <55843965+jrh16u@users.noreply.github.com>
2019-11-14 19:00:35 -0500

    Attempted code to populate data
    Used papa parse npm package to attempt to parse local csv file to populate data in the chart.

--- Chart.js
+++ Chart.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
 import Title from './Title';
+import Papa from 'papaparse';
+
+var data;
+Papa.parse('DateDamage.csv', {
+  header: true,
+  download: true,
+  dynamicTyping: true,
+  complete: function(results){
+    console.log(results);
+    data = results.data;
+  }
+});
 
 // Generate Sales Data
-function createData(time, amount) {
-  return { time, amount };
+function createData(Date, Damage) {
+  return { Date, Damage };
 }
 
-const data = [
-  createData('00:00', 0),
+//Broken parse code for csv file DateDamage.csv
+//File = fetch('DateDamage.csv')
+/*const data = [
+ forEach(i => {
+    const col=i.split(',');
+    const Date=col[0];
+    const Damage=col[1];
+    createData(Date, Damage)
+  }];
+/*  createData('00:00', 0),
   createData('03:00', 300),
   createData('06:00', 600),
   createData('09:00', 800),
@@ -16,8 +36,8 @@ const data = [
   createData('15:00', 2000),
   createData('18:00', 2400),
   createData('21:00', 2400),
-  createData('24:00', undefined),
-];
+  createData('24:00', undefined),*/
+//];
 
 export default function Chart() {
   return (
@@ -33,13 +53,13 @@ export default function Chart() {
             left: 24,
           }}
         >
-          <XAxis dataKey="time" />
+          <XAxis dataKey="Date" />
           <YAxis>
             <Label angle={270} position="left" style={{ textAnchor: 'middle' }}>
               Damage ($MIL)
             </Label>
           </YAxis>
-          <Line type="monotone" dataKey="amount" stroke="#556CD6" dot={false} />
+          <Line type="monotone" dataKey="Damage" stroke="#556CD6" dot={false} />
         </LineChart>
       </ResponsiveContainer>
     </React.Fragment>
