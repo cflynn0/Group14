c2fe36a10e92172fc3428006d7d6008e71ba115c
ryanratkovich <37816346+ryanratkovich@users.noreply.github.com>
2019-12-05 23:15:25 -0500

    Completed necessary files to link User Reporting to Firebase
    User reports now write to DB and are read back to web page in real time.

--- Form.js
+++ Form.js
@@ -1,39 +1,90 @@
 import React, { useState } from "react";
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import firebase from './firebase';
 
 function Form() {
   const [firstName, setFirstName] = useState("");
   const [location, setLocation] = useState("");
   const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
 
-  const onSubmit = values => console.log(values);
+  function onSubmit(e) {
+    e.preventDefault()
+
+    firebase.firestore().collection('entries').add({
+      firstName,
+      location,
+      description,
+      date,
+    })
+    .then( () => {
+      setFirstName('')
+      setLocation('')
+      setDescription('')
+      setDate('')
+    })
+  }
 
   return (
     <form onSubmit={onSubmit}>
-      <input
+      <TextField
         value={firstName}
-        onChange={e => setFirstName(e.target.value)}
-        placeholder="First name"
-        type="text"
+        onChange={e => setFirstName(e.currentTarget.value)}
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="firstName"
+        label="First name"
         name="firstName"
-        required
+        autoComplete="firstName"
+        autoFocus
       />
-      <input
+      <TextField
         value={location}
-        onChange={e => setLocation(e.target.value)}
-        placeholder="Location"
-        type="text"
-        name="location"
+        onChange={e => setLocation(e.currentTarget.value)}
+        variant="outlined"
+        margin="normal"
         required
+        fullWidth
+        id="zip"
+        label="Zip code"
+        name="zip"
+        autoComplete="zip"
       />
-      <input
+      <TextField
         value={description}
-        onChange={e => setDescription(e.target.value)}
-        placeholder="Description"
-        type="text"
-        name="description"
+        onChange={e => setDescription(e.currentTarget.value)}
+        variant="outlined"
+        margin="normal"
         required
+        fullWidth
+        id="description"
+        label="What's your weather like?"
+        name="description"
+        autoComplete="description"
       />
-      <button type="submit">Submit</button>
+      <TextField
+        value={date}
+        onChange={e => setDate(e.currentTarget.value)}
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="date"
+        label="MM/DD/YYYY"
+        name="date"
+        autoComplete="date"
+      />
+
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary">
+        Submit
+      </Button>
     </form>
   );
 }
